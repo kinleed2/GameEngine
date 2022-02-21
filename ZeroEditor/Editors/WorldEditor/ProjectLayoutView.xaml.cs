67aed80494a8d53b51192748287acff4bff586e5
kinleed2 <kinleego@gmail.com>
2022-02-22 02:15:47 +0900

    Implement game entity and componets in editor.

--- ZeroEditor/Editors/WorldEditor/ProjectLayoutView.xaml.cs
+++ ZeroEditor/Editors/WorldEditor/ProjectLayoutView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ZeroEditor.Components;
 using ZeroEditor.GameProject;
 
 namespace ZeroEditor.Editors
@@ -24,6 +25,19 @@ namespace ZeroEditor.Editors
         public ProjectLayoutView()
         {
             InitializeComponent();
+        }
+
+        private void OnAddGameEntity_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            var vm = btn.DataContext as Scene;
+            vm.AddGameEntityCommand.Execute(new GameEntity(vm){ Name = "Empty Game Entity"});
+        }
+
+        private void OnGameEntities_ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var entity = (sender as ListBox).SelectedItems[0];
+            GameEntityView.Instance.DataContext = entity;
         }
     }
 }
--- ZeroEditor/GameProject/Scene.cs
+++ ZeroEditor/GameProject/Scene.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using ZeroEditor.Components;
+using ZeroEditor.Utilities;
 
 namespace ZeroEditor.GameProject
 {
@@ -42,11 +46,62 @@ namespace ZeroEditor.GameProject
             }
         }
 
+        public ICommand AddGameEntityCommand { get; private set; }
+        public ICommand RemoveGameEntityCommand { get; private set; }
+
+        [DataMember(Name = nameof(GameEntities))]
+        private readonly ObservableCollection<GameEntity> _gameEntities = new ObservableCollection<GameEntity>();
+        public ReadOnlyObservableCollection<GameEntity> GameEntities { get; private set; }
+
+        private void AddGameEntity(GameEntity entity)
+        {
+            Debug.Assert(!_gameEntities.Contains(entity));
+            _gameEntities.Add(entity);
+        }
+
+        private void RemoveGameEntity(GameEntity entity)
+        {
+            Debug.Assert(_gameEntities.Contains(entity));
+            _gameEntities.Remove(entity);
+        }
+
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext context)
+        {
+            if (_gameEntities != null)
+            {
+                GameEntities = new ReadOnlyObservableCollection<GameEntity>(_gameEntities);
+                OnPropertyChanged(nameof(GameEntities));
+            }
+
+            AddGameEntityCommand = new RelayCommand<GameEntity>(x =>
+            {
+                AddGameEntity(x);
+                var entityIndex = _gameEntities.Count - 1;
+
+                Project.UndoRedo.Add(new UndoRedoAction(
+                    () => RemoveGameEntity(x),
+                    () => _gameEntities.Insert(entityIndex, x),
+                    $"Add {x.Name} to {Name}"));
+            });
+
+            RemoveGameEntityCommand = new RelayCommand<GameEntity>(x =>
+            {
+                var entityIndex = _gameEntities.IndexOf(x);
+                RemoveGameEntity(x);
+                Project.UndoRedo.Add(new UndoRedoAction(
+                    () => _gameEntities.Insert(entityIndex, x),
+                    () => RemoveGameEntity(x),
+                    $"Remove {x.Name} from {Name}"));
+            });
+        }
+
         public Scene(Project project, string name)
         {
             Debug.Assert(project != null);
             Name = name;
             Project = project;
+            OnDeserialized(new StreamingContext());
         }
     }
 }
